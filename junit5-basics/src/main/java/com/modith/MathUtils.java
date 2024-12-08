757a49cfd0825b063619e5cc9b44cba2aa3c3665
modith-prabath <mod9prabath@gmail.com>
2024-12-08 12:12:02 +0530

    tested assert throws with surefire plugin

--- junit5-basics/src/main/java/com/modith/MathUtils.java
+++ junit5-basics/src/main/java/com/modith/MathUtils.java
@@ -5,4 +5,13 @@ public class MathUtils {
     public int add(int a, int b){
         return a+b;
     }
+    
+    public double computeAreaOfCircle(double radius) {
+    	return Math.PI * radius * radius ;
+    }
+    
+    public int divide(int a , int b) {
+    	return a/b;
+    }
+    
 }
--- junit5-basics/src/test/java/com/modith/MathUtilsTest.java
+++ junit5-basics/src/test/java/com/modith/MathUtilsTest.java
@@ -3,6 +3,7 @@ package com.modith;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 class MathUtilsTest {
 	
@@ -14,6 +15,17 @@ class MathUtilsTest {
 	@Test
 	void test() {
 		assertEquals(expected, actual, "The add method should add two numbers");
+	}
+	
+	
+	@Test
+	void testArea() {
+		assertEquals(314.1592653589793, m.computeAreaOfCircle(10), "Calc radius of circle");
+	}
+	
+	@Test
+	void testDivide() {
+		assertThrows(ArithmeticException.class, () -> m.divide(1, 0)); 
 	}
 
 }
