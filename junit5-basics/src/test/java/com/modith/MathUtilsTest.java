3ffaeb1ea869c6e08d31bfa445d5b5b746523bf7
modith-prabath <mod9prabath@gmail.com>
2024-12-08 12:57:51 +0530

    added lifecycle hooks

--- junit5-basics/src/test/java/com/modith/MathUtilsTest.java
+++ junit5-basics/src/test/java/com/modith/MathUtilsTest.java
@@ -2,18 +2,48 @@ package com.modith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 class MathUtilsTest {
 	
-	MathUtils m = new MathUtils();
+	MathUtils m; 
 	
-	int expected = 3 ;
-	int actual = m.add(1, 2);
+	@BeforeAll
+	//Reasn to add static keyword here is because 
+	//this will be executed even before class instance is being created
+	//so we need to make it static, withut it it fails
+	static void beforeAll() {
+		System.out.println("Executes before all");
+	}
+	
+	@BeforeEach
+	//init gets initialise before every test method.
+	void init() {
+		System.out.println("Executes before every method");
+		m = new MathUtils();
+	}
+	
+	@AfterEach
+	void afterEach() {
+		System.out.println("After each method is exec");
+	}
+	
+	@AfterAll
+	//Similar to before all
+	static void afterAll() {
+		System.out.println("AfterAll are executed");
+	}
+	
+	
 
 	@Test
-	void test() {
+	void testAdd() {
+		int expected = 3 ;
+		int actual = m.add(1, 2);
 		assertEquals(expected, actual, "The add method should add two numbers");
 	}
 	
